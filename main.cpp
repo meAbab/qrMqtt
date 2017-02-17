a6a591bc12a9b5da4b2686447455da9530235b2a
Kaj Van der Hallen <kaj.vanderhallen@tothepoint.company>
2017-02-17 16:09:30 +0100

    Fix: Uncommented variable declaration

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**
 * @file main.cpp
 * call camera, call publisher to send
-* msg to subscriber, waits for 5 sec, 
+* msg to subscriber, waits for 5 sec,
 * and recall camera.
 */
 
@@ -19,7 +19,7 @@ int main(void)
     {
 
 	class qrMqtt *qr2sp;
-	//int rc;
+	int rc;
 	mosqpp::lib_init();
 
 	qr2sp = new qrMqtt("qr2sp", "pcktatDoor", "192.168.178.100", 1883);
@@ -36,7 +36,7 @@ int main(void)
 	    {
 			qr2sp->reconnect();
 	    }
-	    /** 
+	    /**
 		* will wait 5 sec, so camera light will be turned off
 		* for 5 seconds. After 5 sec, camera will active again to read QR-code
 		*/
@@ -53,17 +53,17 @@ int main(void)
 }
 
 /*
-Once cake sent to us, bought back to package-center and next 2 days, we were not able to pick it. 
+Once cake sent to us, bought back to package-center and next 2 days, we were not able to pick it.
 However at the same day we were rounding near by package-center for few times.
 
 While get back home, it was late and 2 days later we'd a cake with feet-smell.
 
-Anyway, I guess a door-bell with QR-code reader that somehow 
+Anyway, I guess a door-bell with QR-code reader that somehow
 connected with smart phone [mqtt i used here] would be helpful for package owner.
 
 For future idea, a packet box can build infront of door,
-and order id [that you made order in ebay, amazon, aliexpress etc], 
-description may supplied to MQTT broker, and once QR-code scanned with 
+and order id [that you made order in ebay, amazon, aliexpress etc],
+description may supplied to MQTT broker, and once QR-code scanned with
 supplied order-id and description, the packet box door will open.
 The postman will put the packet and close it.
 
